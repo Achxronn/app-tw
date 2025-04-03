58d45f4910f397f4ca2c3504f421ed5e2a2b476a
Nianis Watanasiriwanich <nianis612@gmail.com>
2025-04-03 10:29:11 +0700

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function MyApp() {
-  return (
-  <div>
-  <h1>Hello world</h1>
-  <p>Lorem ipsum dolor sit amet.</p>
+  return (<div>
+
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
   </div>
   )
 }
